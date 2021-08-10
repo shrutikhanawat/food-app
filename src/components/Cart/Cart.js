2ec53b76a2a0e7d94540724ee290940ef612eb36
Shruti Bhati <shrutibhati007@gmail.com>
2021-08-10 21:13:07 +0530

    initial commit

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,60 @@
+import { useContext,useState } from "react";
+import Modal from "../UI/Modal";
+import classes from './Cart.module.css';
+import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem/CartItem";
+import Checkout from "./Checkout";
+
+const Cart = (props)=>{
+
+    const cartCtx = useContext(CartContext);
+    const [isCheckout, setIsCheckout]= useState();
+    
+    const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+    const hasItems = cartCtx.items.length >0 ;
+     const cartItemRemoveHandler=(id)=>{
+         cartCtx.removeItem(id);
+
+     };
+     const cartItemAddHandler = (item)=>{
+         cartCtx.addItem(item);
+
+     };
+const orderHandler= ()=>{
+    setIsCheckout(true);
+}
+
+    const cartItem=(<ul className ={classes['cart-items']}>
+        {cartCtx.items.map((item) =>(
+<CartItem   
+    key ={item.id}
+    name = {item.name}
+    amount = {item.amount}
+    price= {item.price}
+    onRemove ={cartItemRemoveHandler.bind(null,item.id)}
+    onAdd= {cartItemAddHandler.bind(null, item)}
+/>
+ ))}
+    </ul>
+
+    );
+   const modalActions =  
+   <div className={classes.actions}>
+   <button className ={classes['button--alt']} onClick={props.onClose}>Close</button>
+  {hasItems && <button className ={classes.button} onClick={orderHandler}>Order</button>}
+</div>
+    
+    return (
+        <Modal onClose={props.onClose}>
+            {cartItem}
+            <div className={classes.total}>
+                <span>Total Amount</span>
+                <span>{totalAmount}</span>
+            </div>
+            {isCheckout && <Checkout onCancel={props.onClose}/>}
+            {!isCheckout && modalActions}
+            
+            </Modal>
+    );
+};
+export default Cart;
---
+++ src/components/Layout/Header.js
@@ -0,0 +1,22 @@
+import {Fragment} from 'react';
+import mealImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
+
+const Header= (props) => {
+    return (
+    <Fragment>
+    <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton onClick={props.onShowCart}/> 
+        </header>
+    <div className={classes['main-image']}>
+    <img src={mealImage} alt="A table of full of delicious food"/>
+    </div>
+    
+    
+    </Fragment>
+    );
+
+};
+export default Header;
